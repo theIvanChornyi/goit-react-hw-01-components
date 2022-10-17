d1db176a4528c5090252c730f8e85702046a577f
Ivan Chornyi <theivanchornyi@gmail.com>
2022-10-17 15:23:44 +0300

    change emails

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,27 @@
-import PropTypes from 'prop-types';
+import { Profile } from 'components/Profile';
+import { Statistics } from 'components/Statistics';
+import { FriendList } from 'components/FriendList';
+import { TransactionHistory } from 'components/TransactionHistory';
 
-export const App = ({ children }) => {
-  return <main>{children}</main>;
-};
+import user from 'data/user.json';
+import data from 'data/data.json';
+import friends from 'data/friends.json';
+import transactions from 'data/transactions.json';
 
-App.prototype = {
-  children: PropTypes.node,
+export const App = () => {
+  return (
+    <main>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </main>
+  );
 };
--- src/index.js
+++ src/index.js
@@ -6,32 +6,11 @@ import './index.css';
 import { theme } from 'theme';
 
 import { App } from 'components/App';
-import { Profile } from 'components/Profile';
-import { Statistics } from 'components/Statistics';
-import { FriendList } from 'components/FriendList';
-import { TransactionHistory } from 'components/TransactionHistory';
-
-import user from 'data/user.json';
-import data from 'data/data.json';
-import friends from 'data/friends.json';
-import transactions from 'data/transactions.json';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App>
-        <Profile
-          username={user.username}
-          tag={user.tag}
-          location={user.location}
-          avatar={user.avatar}
-          stats={user.stats}
-        />
-        <Statistics title="Upload stats" stats={data} />
-        <Statistics stats={data} />
-        <FriendList friends={friends} />
-        <TransactionHistory items={transactions} />
-      </App>
+      <App />
     </ThemeProvider>
   </React.StrictMode>
 );
